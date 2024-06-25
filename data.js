28d1f181296a5447c40d1186fef590092c935fa2
unknown <rafael.reis.git@gmail.com>
2024-06-25 09:47:18 -0300

    primeiro commit

---
+++ data.js
@@ -0,0 +1,169 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sala-de-estar-demo",
+      "name": "Sala de Estar DEMO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8683963524195413,
+          "pitch": 0.12478824473783945,
+          "rotation": 1.5707963267948966,
+          "target": "1-sute-1-demo"
+        },
+        {
+          "yaw": -2.394254003271339,
+          "pitch": 0.1416528797870118,
+          "rotation": 4.71238898038469,
+          "target": "2-quarto-2-demo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sute-1-demo",
+      "name": "Suíte 1 DEMO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 1.9871484416162186,
+        "pitch": 0.028209391135138873,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5706964044959939,
+          "pitch": 0.158858788947299,
+          "rotation": 4.71238898038469,
+          "target": "3-banheiro-luxo-demo-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-quarto-2-demo",
+      "name": "Quarto 2 DEMO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.643761457325251,
+          "pitch": 0.11575665668135393,
+          "rotation": 1.5707963267948966,
+          "target": "0-sala-de-estar-demo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-banheiro-luxo-demo-",
+      "name": "Banheiro Luxo DEMO ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2416024696460273,
+          "pitch": 0.056050203016495104,
+          "rotation": 1.5707963267948966,
+          "target": "0-sala-de-estar-demo"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Tour 360 DEMO",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
